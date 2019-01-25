f750231a635ada9768ffcecb960eb2d3041cd619
sebastianbukowski <seb-27@o2.pl>
2019-01-25 22:54:05 +0100

    Add timeConverter module

--- modules/GetOSinfo.js
+++ modules/GetOSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var timeConverter = require("../modules/TimeConvert");
 function getOSinfo() {
     var type = os.type();
     if(type === 'Darwin') {
@@ -10,13 +11,10 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    var hours = Math.floor(uptime/3600);
-    var minutes = Math.floor((uptime - hours*3600)/60);
-    var seconds = uptime - (hours*3600) - (minutes*60);
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ', uptime + 's'+' is: ' + hours + 'h'+':' + minutes + 'min'+ ':' + seconds + 's');
+    console.log('Uptime: ', uptime + 's'+' is: ' + timeConverter.convert(uptime));
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
